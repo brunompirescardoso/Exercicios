b5a54ef38ed9f882792449bb81ac5bbd055bf835
Bruno M. <brunompirescardoso@gmail.com>
2024-07-29 20:51:24 -0300

    feat(): Adiciona exercicio numero 4 Seu obejtivo eh imprimir resultado de operacoes diversas

---
+++ src/br/com/main/projeto/exercicios/basicos/N4.java
@@ -0,0 +1,13 @@
+package br.com.main.projeto.exercicios.basicos;
+
+
+
+//Escreva um programa Java para imprimir os resultados das seguintes operações.
+public class N4 {
+    public static void main(String[] args) {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + (-3) * 5 / 8);
+        System.out.println( 5 + 15 / 3 * 2 -8 % 3);
+    }
+}
