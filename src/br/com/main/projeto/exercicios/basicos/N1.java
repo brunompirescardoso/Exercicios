19a1a3d0b4ba2b3caea7845b4a47cc2f8b8b6b9f
Bruno M. <brunompirescardoso@gmail.com>
2024-07-29 20:30:19 -0300

    feat(): Adiciona o primeiro exercicio A função é imprimir frases específicas

---
+++ src/br/com/main/projeto/exercicios/basicos/N1.java
@@ -0,0 +1,16 @@
+package br.com.main.projeto.exercicios.basicos;
+
+
+/*1. Escreva um programa Java para imprimir 'Hello' na tela e seu nome em uma linha separada.
+Saída esperada :
+Olá
+Alexandra Abramov (tradução)*/
+public class N1 {
+    public static void main(String[] args) {
+        String ola = "Hello";
+        String nome = "Bruno Maximiliano Pires Cadoso";
+
+        System.out.println(ola + "\n" + nome);
+
+    }
+}
