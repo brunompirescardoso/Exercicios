38b640d393af1fec44e5979c053c5ab82955d40d
Bruno M. <brunompirescardoso@gmail.com>
2024-07-29 20:33:58 -0300

    feat(): Adiciona o exercicio numero 2 Seu objetivo eh imprimir uma soma

---
+++ src/br/com/main/projeto/exercicios/basicos/N2.java
@@ -0,0 +1,18 @@
+package br.com.main.projeto.exercicios.basicos;
+
+
+
+/*
+Escreva um programa Java para imprimir a soma de dois números.
+Dados de teste:
+74 + 36
+Saída esperada :
+110
+ */
+public class N2 {
+    public static void main(String[] args) {
+        int soma = 74 + 36;
+
+        System.out.println(soma);
+    }
+}
