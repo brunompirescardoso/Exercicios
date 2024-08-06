75f68f523e7adc4cd093e72ba9d3e45debf95389
Bruno M. <brunompirescardoso@gmail.com>
2024-08-05 21:17:11 -0300

    feat(): Adiciona o exercicio numero 8 Imprime na tela uma sequencia de letras pre estabelecida

---
+++ src/br/com/main/projeto/exercicios/basicos/N8.java
@@ -0,0 +1,16 @@
+package br.com.main.projeto.exercicios.basicos;
+
+/*Write a Java program to display the following pattern.
+Sample Pattern :
+
+   J    a   v     v  a
+   J   a a   v   v  a a
+J  J  aaaaa   V V  aaaaa
+ JJ  a     a   V  a     a
+ */
+public class N8 {
+    public static void main(String[] args){
+        System.out.println("   J    a   v     v  a\n   J   a a   v   v  a a\nJ  J  aaaaa   V V  aaaaa\n JJ  a     a   V  a     a");
+
+    }
+}
