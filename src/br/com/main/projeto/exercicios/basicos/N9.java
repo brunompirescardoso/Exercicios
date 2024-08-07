0ecd969b37d4706f2888dc8e6892b157619bff6b
Bruno M. <brunompirescardoso@gmail.com>
2024-08-07 19:54:58 -0300

    feat(): Adiciona exercicio numero 9 Mostra resultado de expressao matematica

---
+++ src/br/com/main/projeto/exercicios/basicos/N9.java
@@ -0,0 +1,11 @@
+package br.com.main.projeto.exercicios.basicos;
+/*
+Write a Java program to compute the specified expressions and print the output.
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+ */
+public class N9 {
+    public static void main(String[] args){
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
